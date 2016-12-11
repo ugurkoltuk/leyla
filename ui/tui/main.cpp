4771a40c369b140e630ab5545280376371857f8e
Hayri Ugur Koltuk <ugurkoltuk@gmail.com>
2016-12-12 00:13:51 +0100

    hc and cc modes added.

--- ui/tui/main.cpp
+++ ui/tui/main.cpp
@@ -3,6 +3,7 @@
 #include <termcap.h>
 #include <unistd.h>
 #include <ctime>
+#include <cstring>
 
 #include "game/gameplay.h"
 #include "ai/leyla.h"
@@ -19,16 +20,43 @@ void clear_screen()
     fputs(str, stdout);
 }
 
+enum Mode
+{
+    Mode_HumanVsComputer,
+    Mode_ComputerVsComputer
+};
+
+void printUsage(void)
+{
+    cerr << "Usage: reversi-cpp <hc|cc> <search depth>" << endl;
+    cerr << "hc for human vs computer, cc for computer vs computer." << endl;
+}
+
 int main(int argc, char **argv)
 {
-    Gameplay game;
-
-    if (argc < 2) {
-        cerr << "Usage: " << argv[0] << " <search depth>" << endl;
+    if (argc < 3) {
+        printUsage();
         exit(EXIT_FAILURE);
     }
 
-    Leyla leyla(atoi(argv[1]), Gameplay::Player_White);
+    Mode gameMode;
+
+    if (!strcmp(argv[1], "hc"))
+    {
+        gameMode = Mode_HumanVsComputer;
+    }
+    else if (!strcmp(argv[1], "cc"))
+    {
+        gameMode = Mode_ComputerVsComputer;
+    }
+    else
+    {
+        printUsage();
+        exit(EXIT_FAILURE);
+    }
+
+    Gameplay game;
+    Leyla leyla(atoi(argv[2]), Gameplay::Player_White);
     bool humanPlayed = false;
     bool leylaPlayed = false;
 
@@ -40,58 +68,58 @@ int main(int argc, char **argv)
     clock_gettime(CLOCK_MONOTONIC, &tm);
     while (!game.hasEnded())
     {
-#if 0
-        size_t row, col;
-        char col_char;
+        if (gameMode == Mode_HumanVsComputer) {
+            size_t row, col;
+            char col_char;
 
-        do {
+            do {
+                clear_screen();
+                if (!game.hasValidMoves())
+                {
+                    cout <<"Human has no valid moves!" << endl;
+                    sleep(3);
+                    game.skip();
+                    break;
+                }
+
+                if (humanPlayed)
+                {
+                    cout << "Invalid move (" << row << ", " << col_char << ")! Try again." << endl;
+                }
+                humanPlayed = true;
+
+                if (leylaPlayed)
+                {
+                    cout << "Leyla played: (" << leylaCoordinates.first << ", " << char(leylaCoordinates.second + 'A') << ")." << endl;
+                }
+                cout << game;
+                cout << "Enter your move:" << endl;
+                cin >> row >> col_char;
+                col = toupper(col_char) - 'A';
+            }
+            while (!game.play(Board::Coordinates(row, col)));
             clear_screen();
+            cout << game;
+        }
+        else if (gameMode == Mode_ComputerVsComputer)
+        {
+            Leyla leyla2(atoi(argv[2]), Gameplay::Player_Black);
+
             if (!game.hasValidMoves())
             {
-                cout <<"Human has no valid moves!" << endl;
-                sleep(3);
+                cout << "Leyla2 has no valid moves." << endl;
                 game.skip();
-                break;
+                continue;
             }
-
-            if (humanPlayed)
-            {
-                cout << "Invalid move (" << row << ", " << col_char << ")! Try again." << endl;
-            }
-            humanPlayed = true;
-
-            if (leylaPlayed)
-            {
-                cout << "Leyla played: (" << leylaCoordinates.first << ", " << char(leylaCoordinates.second + 'A') << ")." << endl;
-            }
+            cout << "Leyla2 is thinking ... " << endl;
+            game.play(leylaCoordinates = leyla2.play(game));
+            clear_screen();
             cout << game;
-            cout << "Enter your move:" << endl;
-            cin >> row >> col_char;
-            col = toupper(col_char) - 'A';
         }
-        while (!game.play(Board::Coordinates(row, col)));
-#endif
-
-        Leyla leyla2(atoi(argv[1]), Gameplay::Player_Black);
-        clear_screen();
-        cout << game;
-
-        if (!game.hasValidMoves())
-        {
-            cout << "Leyla2 has no valid moves." << endl;
-            //sleep(3);
-            game.skip();
-            continue;
-        }
-        cout << "Leyla2 is thinking ... " << endl;
-        game.play(leylaCoordinates = leyla2.play(game));
-        clear_screen();
-        cout << game;
 
         if (!game.hasValidMoves())
         {
             cout << "Leyla has no valid moves." << endl;
-            //sleep(3);
             game.skip();
             continue;
         }
